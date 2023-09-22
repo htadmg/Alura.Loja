e79812e8597fee4b4bd6dabfb12fa478599520b2
Agata Domingues <124289385+htadmg@users.noreply.github.com>
2023-09-22 11:00:35 -0300

    Add project files.

---
+++ Alura.Loja.Testes.ConsoleApp/Produto.cs
@@ -0,0 +1,10 @@
+﻿namespace Alura.Loja.Testes.ConsoleApp
+{
+    internal class Produto
+    {
+        public int Id { get; internal set; }
+        public string Nome { get; internal set; }
+        public string Categoria { get; internal set; }
+        public double Preco { get; internal set; }
+    }
+}
---
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GravarUsandoAdoNet();
+        }
+
+        private static void GravarUsandoAdoNet()
+        {
+            Produto p = new Produto();
+            p.Nome = "Harry Potter e a Ordem da Fênix";
+            p.Categoria = "Livros";
+            p.Preco = 19.89;
+
+            using (var repo = new ProdutoDAO())
+            {
+                repo.Adicionar(p);
+            }
+        }
+    }
+}
